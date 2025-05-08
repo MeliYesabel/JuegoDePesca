76ff2384676fd39d1db979c3671f2d95d7ada41b
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2025-05-08 04:35:07 -0300

    Se hizo el test simple del controladorRegistro y su vista de registro

--- src/main/java/com/tallerwebi/presentacion/ControladorRegistro.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorRegistro.java
@@ -3,16 +3,17 @@ package com.tallerwebi.presentacion;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControladorRegistro {
 
-    @RequestMapping("/registro")
+    @RequestMapping(value = "/registro", method = RequestMethod.GET)
     public ModelAndView irARegistro() {
         ModelMap modeloRegistro = new ModelMap();
         modeloRegistro.put("exito","estas en registro");
 
-        return new ModelAndView("loginPescador", modeloRegistro);
+        return new ModelAndView("registro", modeloRegistro);
     }
 }
