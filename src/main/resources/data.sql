75ac42caf7fa264b9a36d992ba24fd28f58c4688
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2025-06-16 19:54:59 -0300

    cambiando hash validos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,8 @@ INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.e
 
 
 INSERT INTO UsuarioAuth(id, email, password, tipo_usuario) values
-(1,'bart@gmail.com','$2a$10$5LyYW4YW5D0TtHSEpVSMAetUh1qdMtmKaqm51nmFCR8sONDAT.cSm','JUGADOR'),
-(2,'admin@mail.com', '$2a$10$5LyYW4YW5D0TtHSEpVSMAetUh1qdMtmKaqm51nmFCR8sONDAT.cSm','ADMIN');
+(1,'bart@gmail.com','$2a$10$p.KwA3ZiYt1H.sV64X8SOu/WUi4diCbOwfl6S.0VzlWEActSr5BY6','JUGADOR'),
+(2,'admin@mail.com', '$2a$10$IL7Na4ipqDYPWSPigbGh1uGXL2TgVdJp4kocuo2F9bO.Dn4DT.WLC','ADMIN');
 
 INSERT INTO Jugador(id,monedas) VALUES(1,200.0);
 INSERT INTO Admin(id) values (2);
