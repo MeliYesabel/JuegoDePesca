a7dc15855511ddee3616862c99972e74f0223636
angela quelca <anahi.laurani@gmail.com>
2025-06-21 14:56:19 -0300

    redirecciones de seleccion a vista tienda y logros

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,4 @@ INSERT INTO Mar (id_mar,nombre,precio,descripcion,estadoBloqueado) VALUES
 (null,'Mitologia Indu', 300.0, 'mar cinco', TRUE),
 (null,'Mitologia Asteca', 350.0, 'mar seis', TRUE),
 (null,'Mitologia China', 450.0, 'mar siete', TRUE);
+
