26a38f5983afb3719b47bc7fbeaa84a53b08ac4d
Gonzalo Greatti <gonza_lauty@outlook.com>
2025-06-24 21:18:43 -0300

    importe algunas clases que me causaban errores

--- src/main/java/com/tallerwebi/presentacion/ControladorPersonaje.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorPersonaje.java
@@ -1,11 +1,14 @@
 package com.tallerwebi.presentacion;
 
 import com.tallerwebi.dominio.*;
+import com.tallerwebi.dominio.excepcion.ObjetoInexistenteException;
+import com.tallerwebi.dominio.excepcion.ParametroInvalidoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
