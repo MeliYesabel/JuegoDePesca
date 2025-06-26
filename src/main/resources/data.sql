5962d90ebf8849492b0d8a17f295a4a7b6fc6a7b
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2025-06-26 01:37:01 -0300

    Arregle los errores que tenia el data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,39 +13,8 @@ INSERT INTO Admin(id) values (3);
 INSERT INTO Objeto(precioObjeto,nombre)VALUES
 (100.0,'CAÑA MADERA'),
 (150.0,'CAÑA METAL');
-
 --mares
 INSERT INTO Mar (id_mar,nombre,precio,descripcion,estadoBloqueado) VALUES
-(0,'Mitologia griega', 0.0, 'mar uno', FALSE),
-(1,'Mitologia Nordica', 150.0, 'mar dos', TRUE),
-(2,'Mitologia Japonesa', 200.0, 'mar tres', TRUE),
-(3,'Mitologia Yoruba', 250.0, 'mar cuatro', TRUE),
-(4,'Mitologia Indu', 300.0, 'mar cinco', TRUE),
-(5,'Mitologia Asteca', 350.0, 'mar seis', TRUE),
-(6,'Mitologia China', 450.0, 'mar siete', TRUE);
-
-INSERT INTO Rareza (id_rareza, nombre, probabilidadAtrapar, valor) VALUES
-(1, 'Normal', 80.0, 10),
-(2, 'Raro', 50.0, 20),
-(3, 'Épico', 20.0, 40);
-
--- Mar 0 - Mitología griega
-INSERT INTO Pez (id_pez, nombre, id_rareza, id_mar) VALUES
-                                                        (null, 'Delfín Helénico', 1, 0),
-                                                        (null, 'Trucha de Atenea', 1, 0),
-                                                        (null, 'Carpa de Apolo', 1, 0),
-                                                        (null, 'Sirena Menor', 2, 0),
-                                                        (null, 'Hipocampo Menor', 2, 0),
-                                                        (null, 'Kraken Mítico', 3, 0);
-
--- Mar 1 - Mitología Nórdica
-INSERT INTO Pez (id_pez, nombre, id_rareza, id_mar) VALUES
-                                                        (null, 'Pez Runa', 1, 1),
-                                                        (null, 'Salmón de Thor', 1, 1),
-                                                        (null, 'Bacalao de Freyja', 1, 1),
-                                                        (null, 'Serpiente del Fiordo', 2, 1),
-                                                        (null, 'Anguila de Hielo', 2, 1),
-                                                        (null, 'Jörmungandr Joven', 3, 1);
 (1,'Mitologia griega', 0.0, 'mar uno', FALSE),
 (2,'Mitologia Nordica', 150.0, 'mar dos', TRUE),
 (3,'Mitologia Japonesa', 200.0, 'mar tres', TRUE),
@@ -54,6 +23,38 @@ INSERT INTO Pez (id_pez, nombre, id_rareza, id_mar) VALUES
 (6,'Mitologia Asteca', 350.0, 'mar seis', TRUE),
 (7,'Mitologia China', 450.0, 'mar siete', TRUE);
 
+INSERT INTO Rareza (id_rareza, nombre, probabilidadAtrapar, valor) VALUES
+(1, 'Normal', 80.0, 10),
+(2, 'Raro', 50.0, 20),
+(3, 'Épico', 20.0, 40);
+
+-- Mar 1 - Mitología griega
+INSERT INTO Pez (id_pez, nombre, id_rareza, id_mar) VALUES
+                                                        (null, 'Delfín Helénico', 1, 1),
+                                                        (null, 'Trucha de Atenea', 1, 1),
+                                                        (null, 'Carpa de Apolo', 1, 1),
+                                                        (null, 'Sirena Menor', 2, 1),
+                                                        (null, 'Hipocampo Menor', 2, 1),
+                                                        (null, 'Kraken Mítico', 3, 1);
+
+-- Mar 2 - Mitología Nórdica
+INSERT INTO Pez (id_pez, nombre, id_rareza, id_mar) VALUES
+                                                        (null, 'Pez Runa', 1, 2),
+                                                        (null, 'Salmón de Thor', 1, 2),
+                                                        (null, 'Bacalao de Freyja', 1, 2),
+                                                        (null, 'Serpiente del Fiordo', 2, 2),
+                                                        (null, 'Anguila de Hielo', 2, 2),
+                                                       (null, 'Jörmungandr Joven', 3, 2);
+
+--Estos datos estan huerfanos
+/*(1,'Mitologia griega', 0.0, 'mar uno', FALSE),
+(2,'Mitologia Nordica', 150.0, 'mar dos', TRUE),
+(3,'Mitologia Japonesa', 200.0, 'mar tres', TRUE),
+(4,'Mitologia Yoruba', 250.0, 'mar cuatro', TRUE),
+(5,'Mitologia Indu', 300.0, 'mar cinco', TRUE),
+(6,'Mitologia Asteca', 350.0, 'mar seis', TRUE),
+(7,'Mitologia China', 450.0, 'mar siete', TRUE);
+*/
 --tabla jugadorMar jugador:bart@gmail.com contra:Barto123!
 INSERT INTO JugadorMar(id_jugador,id_mar,estadoBloqueado)VALUES
 (2,1,false),
@@ -67,47 +68,47 @@ INSERT INTO JugadorMar(id_jugador,id_mar,estadoBloqueado)VALUES
 
 
 
--- Mar 2 - Mitología Japonesa
+-- Mar 3 - Mitología Japonesa
 INSERT INTO Pez (id_pez, nombre, id_rareza, id_mar) VALUES
-                                                        (null, 'Koi de Amaterasu', 1, 2),
-                                                        (null, 'Trucha Fuji', 1, 2),
-                                                        (null, 'Medaka Sagrada', 1, 2),
-                                                        (null, 'Pez Yokai', 2, 2),
-                                                        (null, 'Siluro de Tormenta', 2, 2),
-                                                        (null, 'Dragón del Agua', 3, 2);
+                                                        (null, 'Koi de Amaterasu', 1, 3),
+                                                        (null, 'Trucha Fuji', 1, 3),
+                                                        (null, 'Medaka Sagrada', 1, 3),
+                                                        (null, 'Pez Yokai', 2, 3),
+                                                        (null, 'Siluro de Tormenta', 2, 3),
+                                                        (null, 'Dragón del Agua', 3, 3);
 
--- Mar 3 - Mitología Yoruba
+-- Mar 4 - Mitología Yoruba
 INSERT INTO Pez (id_pez, nombre, id_rareza, id_mar) VALUES
-                                                        (null, 'Tilapia de Yemayá', 1, 3),
-                                                        (null, 'Mojarra Sagrada', 1, 3),
-                                                        (null, 'Dorado del Sol', 1, 3),
-                                                        (null, 'Pez Orisha', 2, 3),
-                                                        (null, 'Bagre Ancestral', 2, 3),
-                                                        (null, 'Serpiente Marina Olokun', 3, 3);
+                                                        (null, 'Tilapia de Yemayá', 1, 4),
+                                                        (null, 'Mojarra Sagrada', 1, 4),
+                                                        (null, 'Dorado del Sol', 1, 4),
+                                                        (null, 'Pez Orisha', 2, 4),
+                                                        (null, 'Bagre Ancestral', 2, 4),
+                                                        (null, 'Serpiente Marina Olokun', 3, 4);
 
--- Mar 4 - Mitología Hindú
+-- Mar 5 - Mitología Hindú
 INSERT INTO Pez (id_pez, nombre, id_rareza, id_mar) VALUES
-                                                        (null, 'Pez Ganges', 1, 4),
-                                                        (null, 'Barbo de Vishnu', 1, 4),
-                                                        (null, 'Dorado de Shiva', 1, 4),
-                                                        (null, 'Carpa Karma', 2, 4),
-                                                        (null, 'Pez de Loto', 2, 4),
-                                                        (null, 'Makara Legendario', 3, 4);
+                                                        (null, 'Pez Ganges', 1, 5),
+                                                        (null, 'Barbo de Vishnu', 1, 5),
+                                                        (null, 'Dorado de Shiva', 1, 5),
+                                                        (null, 'Carpa Karma', 2, 5),
+                                                        (null, 'Pez de Loto', 2, 5),
+                                                        (null, 'Makara Legendario', 3, 5);
 
--- Mar 5 - Mitología Azteca
+-- Mar 6 - Mitología Azteca
 INSERT INTO Pez (id_pez, nombre, id_rareza, id_mar) VALUES
-                                                        (null, 'Axolote Común', 1, 5),
-                                                        (null, 'Trucha Tenochtitlan', 1, 5),
-                                                        (null, 'Mojarra del Sol', 1, 5),
-                                                        (null, 'Ajolote Guerrero', 2, 5),
-                                                        (null, 'Bagre Jaguar', 2, 5),
-                                                        (null, 'Serpiente Acuática Quetzal', 3, 5);
+                                                        (null, 'Axolote Común', 1, 6),
+                                                        (null, 'Trucha Tenochtitlan', 1, 6),
+                                                        (null, 'Mojarra del Sol', 1, 6),
+                                                        (null, 'Ajolote Guerrero', 2, 6),
+                                                        (null, 'Bagre Jaguar', 2, 6),
+                                                        (null, 'Serpiente Acuática Quetzal', 3, 6);
 
--- Mar 6 - Mitología China
+-- Mar 7 - Mitología China
 INSERT INTO Pez (id_pez, nombre, id_rareza, id_mar) VALUES
-                                                        (null, 'Koi Imperial', 1, 6),
-                                                        (null, 'Carpa Celestial', 1, 6),
-                                                        (null, 'Dorado de Jade', 1, 6),
-                                                        (null, 'Dragón Pez', 2, 6),
-                                                        (null, 'Siluro Dorado', 2, 6),
-                                                        (null, 'Longyu, Espíritu del Agua', 3, 6);
+                                                        (null, 'Koi Imperial', 1, 7),
+                                                        (null, 'Carpa Celestial', 1, 7),
+                                                        (null, 'Dorado de Jade', 1, 7),
+                                                        (null, 'Dragón Pez', 2, 7),
+                                                        (null, 'Siluro Dorado', 2, 7),
+                                                        (null, 'Longyu, Espíritu del Agua', 3, 7);
